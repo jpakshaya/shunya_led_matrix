311b29d984cc4ec99d8c2f13b0590df0a9e947b1
jpakshaya <166060079+jpakshaya@users.noreply.github.com>
2025-06-23 21:29:30 +0530

    Update MAP_COLOR.c

--- main/MAP_COLOR.c
+++ main/MAP_COLOR.c
@@ -7,19 +7,15 @@ void MAP_COLOR(int index, char color){
     switch(color){
 
         case 'R':
-        led_strip_set_pixel(led_strip, index, 255, 0, 0);
+        led_strip_set_pixel(led_strip, index, 50, 0, 0);
         break;
 
         case 'G':
-        led_strip_set_pixel(led_strip, index, 0, 255, 0);
+        led_strip_set_pixel(led_strip, index, 0, 50, 0);
         break;
 
         case 'B':
-        led_strip_set_pixel(led_strip, index, 0, 0, 255);
-        break;
-
-        case 'W':
-        led_strip_set_pixel(led_strip, index, 255, 255, 255);
+        led_strip_set_pixel(led_strip, index, 0, 0, 50);
         break;
         
     }
